224afc6611d6d328bdbc133a3f8f6dbd7726315c
Brooklyn Autism Center <brooklynautismcenter@iMac-SmartBoard.local>
2016-01-22 14:33:21 -0500

    peters first commit

---
+++ Peter_Python.py
@@ -0,0 +1,13 @@
+'''peter hirschfeld 
+mAD libs project'''
+title= "Sally had a little lamb"
+
+print "welcome! This game was created by Peter Hirschfeld"
+print "the name of my game is "+title
+name= raw_input("what is your name?") 
+size= raw_input("big or little? ")
+animal= raw_input("what is your favorite animal?") '
+food1= raw_input("what Do you like to eat?")
+food2=raw_input("what do you not like to eat?")
+print name + " had a " + size +" " + animal
+print name + “ likes to eat an+ apple “ +
